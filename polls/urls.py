3b36d62d786a54240e25188b1da60450ca0759b0
Christian Castillo <castillochristian3@gmail.com>
2023-09-13 12:32:48 -0400

    created detail, results, vote method and mapped to url paths

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #polls/
     path("", views.index, name="index"),
+    #polls/int
+    path("<int:question_id>", views.detail, name="detail"),
+    #polls/int/vote
+    path("<int:question_id>/vote", views.vote, name="vote"),
+    #polls/int/results
+    path("<int:question_id>/results", views.results, name="results"),
 ]
